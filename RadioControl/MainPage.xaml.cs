33cf83ec33a1001666ee278bf65ccc9aae4442cd
Martin Pecka <peci1@seznam.cz>
2018-02-05 03:11:38 +0100

    Initial commit.

---
+++ RadioControl/MainPage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Devices.Radios;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace RadioControl
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<RadioModel> radios = new List<RadioModel>();
+        public static MainPage Current;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // This is a static public property that allows downstream pages to get a handle to the MainPage instance
+            // in order to call methods that are in this class.
+            Current = this;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // RequestAccessAsync must be called at least once from the UI thread
+            var accessLevel = await Radio.RequestAccessAsync();
+            if (accessLevel != RadioAccessStatus.Allowed)
+            {
+                NotifyUser("App is not allowed to control radios.", NotifyType.ErrorMessage);
+            }
+            else
+            {
+                InitializeRadios();
+            }
+        }
+
+        private async void InitializeRadios()
+        {
+            // An alternative to Radio.GetRadiosAsync is to use the Windows.Devices.Enumeration pattern,
+            // passing Radio.GetDeviceSelector as the AQS string
+            this.radios.Clear();
+            var radios = await Radio.GetRadiosAsync();
+            for (int i = 0; i < radios.Count; i++)
+            {
+                var radio = radios[i];
+                var radioItem = new RadioModel(radio, i, this);
+                RadioSwitchList.Items.Add(radioItem);
+                this.radios.Add(radioItem);
+            }
+        }
+
+        /// <summary>
+        /// Display a message to the user.
+        /// This method may be called from any thread.
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="type"></param>
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            // If called from the UI thread, then update immediately.
+            // Otherwise, schedule a task on the UI thread to perform the update.
+            if (Dispatcher.HasThreadAccess)
+            {
+                UpdateStatus(strMessage, type);
+            }
+            else
+            {
+                var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => UpdateStatus(strMessage, type));
+            }
+        }
+
+        private void UpdateStatus(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    break;
+            }
+
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public enum NotifyType
+        {
+            StatusMessage,
+            ErrorMessage
+        };
+    }
+}
---
+++ RadioControl/RadioModel.cs
@@ -0,0 +1,125 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Devices.Radios;
+using Windows.UI.Core;
+
+namespace RadioControl
+{
+    public class RadioModel : INotifyPropertyChanged
+    {
+        private Radio radio;
+        private bool isEnabled = true;
+        private MainPage parent;
+        private int index;
+
+        public RadioModel(Radio radio, int index, MainPage parent)
+        {
+            this.radio = radio;
+            this.index = index;
+            this.parent = parent;
+            this.radio.StateChanged += Radio_StateChanged;
+        }
+
+        private async void Radio_StateChanged(Radio sender, object args)
+        {
+            // The Radio StateChanged event doesn't run from the UI thread, so we must use the dispatcher
+            // to run NotifyPropertyChanged
+            await this.parent.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                NotifyPropertyChanged("IsRadioOn");
+            });
+
+            parent.NotifyUser("Radio " + radio.Name + " is now " + radio.State, MainPage.NotifyType.StatusMessage);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.radio.Name;
+            }
+        }
+
+        public int Index
+        {
+            get
+            {
+                return this.index;
+            }
+        }
+
+        public string Kind
+        {
+            get
+            {
+                return this.radio.Kind.ToString();
+            }
+        }
+
+        public bool IsRadioOn
+        {
+            get
+            {
+                return this.radio.State == RadioState.On;
+            }
+            set
+            {
+                SetRadioState(value);
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                this.isEnabled = value;
+                NotifyPropertyChanged();
+            }
+        } 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private async void SetRadioState(bool isRadioOn)
+        {
+            var radioState = isRadioOn ? RadioState.On : RadioState.Off;
+            Disable();
+            await this.radio.SetStateAsync(radioState);
+            NotifyPropertyChanged("IsRadioOn");
+            Enable();
+        }
+
+        private void Enable()
+        {
+            IsEnabled = true;
+        }
+
+        private void Disable()
+        {
+            IsEnabled = false;
+        }
+    }
+}
